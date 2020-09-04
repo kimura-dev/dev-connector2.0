55f264d6884213b7cb29c5207f9642eb50fddfbe
Scott Reyell <smreyell@gmail.com>
2020-09-04 13:22:17 -0700

    connect mongoDB

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
-
+const connectDB = require('./config/db');
 const app = express();
-
 const PORT = process.env.PORT || 5001;
+
+// Connect Database
+connectDB();
 
 app.listen(PORT, () => console.log(`Server started on port:  ${PORT}`));
 
