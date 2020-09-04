b69e36c5ca24264514972e7df7b090d5e13fc4fd
Scott Reyell <smreyell@gmail.com>
2020-09-04 14:23:56 -0700

    created user request with validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
 
-// @route GET api/users
-// @desc Test route
+// @route POST api/users
+// @desc Register route
 // @access Public
-router.get('/', (req, res) => {
-	res.send('User route');
-});
+router.post(
+	'/',
+	[
+		body('name', 'Name is required').not().isEmpty(),
+		body('email', 'Please include a valid email').isEmail(),
+		body('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })
+	],
+	(req, res) => {
+		// Finds the validation errors in this request and wraps them in an object with handy functions
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+
+		console.log(req.body);
+		res.send(req.body);
+	}
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const PORT = process.env.PORT || 5001;
 // Connect Database
 connectDB();
 
+// Init Middleware
+app.use(express.json({ extended: false }));
+
 app.listen(PORT, () => console.log(`Server started on port:  ${PORT}`));
 
 app.get('/', (req, res) => res.send('API running'));
